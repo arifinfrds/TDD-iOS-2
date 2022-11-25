209339bd7336049ec61c1db6a552849bda2c55a8
arifinfrds <arifinfrds.engineer@gmail.com>
2022-11-25 08:39:21 +0700

    Initial Commit

---
+++ Vidio2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vidio2
+//
+//  Created by Arifin Firdaus on 25/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
