968270a540ee0aad5614549dd141ba7b258774d0
arifinfrds <arifinfrds.engineer@gmail.com>
2022-11-25 08:52:50 +0700

    `LoadVideosFromRemoteUseCase.execute` request items

--- Vidio2Tests/LoadVideosFromRemoteUseCaseTests.swift
+++ Vidio2Tests/LoadVideosFromRemoteUseCaseTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 protocol HTTPClient {
-    
+    func fetchFromAPI(_ url: URLRequest) async throws -> Data
 }
 
 final class LoadVideosFromRemoteUseCase {
@@ -16,6 +16,16 @@ final class LoadVideosFromRemoteUseCase {
     
     init(client: HTTPClient) {
         self.client = client
+    }
+    
+    func execute() async {
+        do {
+            let url = URL(string: "https://vidio.com/api/contents")!
+            let request = URLRequest(url: url)
+            _ = try await client.fetchFromAPI(request)
+        } catch {
+            
+        }
     }
 }
 
@@ -28,11 +38,27 @@ final class LoadVideosFromRemoteUseCaseTests: XCTestCase {
         XCTAssertEqual(client.messages, [])
     }
     
+    func test_execute_requestItems() async {
+        let client = HTTPClientSpy()
+        let sut = LoadVideosFromRemoteUseCase(client: client)
+        
+        await sut.execute()
+        
+        XCTAssertEqual(client.messages, [ .fetchFromAPI ])
+    }
+    
     // MARK: - Helpers
     
     private final class HTTPClientSpy: HTTPClient {
         private(set) var messages = [Message]()
         
-        enum Message: Equatable {}
+        enum Message: Equatable {
+            case fetchFromAPI
+        }
+        
+        func fetchFromAPI(_ url: URLRequest) async throws -> Data {
+            messages.append(.fetchFromAPI)
+            return Data()
+        }
     }
 }
